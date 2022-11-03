f7a85ca1c7733e06998cc1cb2a2a75fa21358803
Hamin Park <hmnpark0@gmail.com>
2022-11-03 12:58:16 +0000

    added cogs

---
+++ cogs/menu.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+from Menu.Menu import Menu
+from datetime import date
+
+
+EXCLUSIONS = {'The Farm Stand/ Salad Bar', 'Ember/Grill', "Farmer's Market", 'Sizzle Grill'}
+COLORS = {
+    'Breakfast': 0xffe100,
+    'Lunch': 0xff9800,
+    'Dinner': 0x651ce3
+}
+REFRESH_TIME = 8
+
+
+def _make_embed(menu, period, date):
+    embed = discord.Embed(title=f'{menu.get("name")}',
+                            description=f'{period} {date}',
+                            color=COLORS[period])
+    for k in menu.get(period):
+        if k not in EXCLUSIONS:
+            embed.add_field(name=k,
+                value=''.join([f'{food}\n' for food in menu.get(period).get(k)]),
+                inline=True)
+    return embed
+
+
+class MenuCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.menu = Menu()
+
+    @commands.command(name='breakfast')
+    async def _breakfast(self, ctx):
+        if self.menu.today != date.today().strftime('%m/%d/%Y') and date.now().hour >= REFRESH_TIME:
+            self.menu.force_update_menu()
+
+        embed = _make_embed(self.menu.brandywine, 'Breakfast', self.menu.today)
+        await ctx.send(embed=embed)
+        embed = _make_embed(self.menu.anteatery, 'Breakfast', self.menu.today)
+        await ctx.send(embed=embed)
+
+    @commands.command(name='lunch')
+    async def _lunch(self, ctx):
+        embed = _make_embed(self.menu.brandywine, 'Lunch', self.menu.today)
+        await ctx.send(embed=embed)
+        embed = _make_embed(self.menu.anteatery, 'Lunch', self.menu.today)
+        await ctx.send(embed=embed)
+
+    @commands.command(name='dinner')
+    async def _dinner(self, ctx):
+        embed = _make_embed(self.menu.brandywine, 'Dinner', self.menu.today)
+        await ctx.send(embed=embed)
+        embed = _make_embed(self.menu.anteatery, 'Dinner', self.menu.today)
+        await ctx.send(embed=embed)
+
+    @commands.command(name='refresh')
+    @commands.is_owner()
+    async def _refresh(self, ctx):
+        self.menu.force_update_menu()
+        await ctx.send('Refreshed menu!')
+
+
+async def setup(bot):
+    await bot.add_cog(MenuCog(bot))
---
+++ cogs/owner.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+
+
+# def is_privileged():
+#     def predicate(interaction: discord.Interaction) -> bool:
+#         return interaction.user.id == 135960976863264770
+#     return app_commands.check(predicate)
+
+
+
+
+class OwnerCog(commands.Cog, name='owner'):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(hidden=True, name='shutdown')
+    @commands.is_owner()
+    async def _shutdown(self, ctx):
+        await ctx.send('sayonara')
+        await ctx.bot.close()
+
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def load(self, ctx, *, module : str):
+        """Loads a module."""
+        try:
+            self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.send('\N{PISTOL}')
+            await ctx.send('{}: {}'.format(type(e).__name__, e))
+        else:
+            await ctx.send('\N{OK HAND SIGN}')
+
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def unload(self, ctx, *, module : str):
+        """Unloads a module."""
+        try:
+            self.bot.unload_extension(module)
+        except Exception as e:
+            await ctx.send('\N{PISTOL}')
+            await ctx.send('{}: {}'.format(type(e).__name__, e))
+        else:
+            await ctx.send('\N{OK HAND SIGN}')
+
+
+    @commands.command(name='reload', hidden=True)
+    @commands.is_owner()
+    async def _reload(self, ctx, *, module : str):
+        """Reloads a module."""
+        try:
+            await self.bot.unload_extension(module)
+            await self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.send('\N{PISTOL}')
+            await ctx.send('{}: {}'.format(type(e).__name__, e))
+        else:
+            await ctx.send('\N{OK HAND SIGN}')
+
+
+async def setup(bot):
+    await bot.add_cog(OwnerCog(bot))
