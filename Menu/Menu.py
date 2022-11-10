44657c531256e66566c5b312d118a7b8e236f05c
Hamin Park <hmnpark0@gmail.com>
2022-11-10 11:19:06 +0000

    Menu.today: str -> Menu.time: datetime.datetime

--- Menu/Menu.py
+++ Menu/Menu.py
@@ -9,15 +9,18 @@ TIMEZONE = datetime.timezone(-datetime.timedelta(hours=8))
 
 
 class Menu:
+    time: dt
+    brandywine: dict
+    anteatery: dict
+
     def __init__(self):
-        self.today = _get_today_as_str()
         self.brandywine = {'name': 'Brandywine', 'Breakfast': dict(), 'Lunch': dict(), 'Dinner': dict()}
         self.anteatery = {'name': 'The Anteatery', 'Breakfast': dict(), 'Lunch': dict(), 'Dinner': dict()}
-        self._fetch_menus()
+        self._fetch_menus() # self.time is set in here
 
     def update_menu(self):
-        if self.today != _get_today_as_str():
-            self.today = _get_today_as_str()
+        if self.time != dt.date():
+            self.time = dt.date()
             self._fetch_menus()
 
     def force_update_menu(self):
@@ -25,24 +28,23 @@ class Menu:
         Force updates all menus.
         :return:
         """
-        self.today = _get_today_as_str()
         self._fetch_menus()
+
+    def today_as_str(self):
+        return self.time.date().strftime('%m/%d/%Y')
 
     def _fetch_from(self, menu: dict, name: str):
         for period in ['Breakfast', 'Lunch', 'Dinner']:
-            menu[period] = _menu(name, period, self.today)
+            menu[period] = _menu(name, period, self.today_as_str())
             if menu[period] == None:
                 menu[period] = {'Uh oh!': ['Something went wrong']}
 
     def _fetch_menus(self):
+        self.time = dt.today().astimezone(tz=TIMEZONE)
         self._fetch_from(self.brandywine, 'Brandywine')
         self._fetch_from(self.anteatery, 'The Anteatery')
 
 
-def _get_today_as_str():
-    return dt.today().astimezone(tz=TIMEZONE).strftime('%m/%d/%Y')
-
-
-def _menu(location, period='Lunch', menu_date=_get_today_as_str()):
+def _menu(location, period='Lunch', menu_date=dt.today().astimezone(tz=TIMEZONE).strftime('%m/%d/%Y')):
     return parse_menu(
         get_menu_from_campusdish_api(LOCATIONS[location], menu_date, PERIODS[period]))
